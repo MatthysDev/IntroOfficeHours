b974c6da6e4fe55dfcd286ae0d661e130f379fdb
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-02-13 16:11:26 +0100

    add github star with api

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,3 +1,5 @@
+import {useEffect} from 'react';
+import {useState} from 'react';
 import React from 'react';
 import {interpolate, spring, useCurrentFrame, useVideoConfig} from 'remotion';
 
@@ -9,39 +11,46 @@ export const Logo: React.FC = () => {
 		fps,
 		frame,
 		config: {
-			damping: 200,
+			damping: 300,
 		},
 		durationInFrames: 30,
 	});
 
-	const entranceOffset = interpolate(entrance, [0, 1], [height, 0]);
+	const entranceOffset = interpolate(entrance, [0, 0.85], [height, 0]);
 
-	const wave1 = Math.cos(frame / 15) * 10 + entranceOffset;
-	const wave2 = Math.cos((frame - 5) / 15) * 10 + entranceOffset;
-
+	const wave2 = Math.cos(frame / 15) * 13 + entranceOffset;
+	const wave1 = Math.cos((frame - 5) / 15) * 16 + entranceOffset;
+	const [stargazersCount, setStargazersCount] = useState(0);
+	useEffect(() => {
+		fetch('https://api.github.com/repos/flexbox/react-native-bootcamp')
+			.then((response) => response.json())
+			.then((data) => {
+				setStargazersCount(data.stargazers_count);
+			});
+	}, []);
+	const opacity = interpolate(frame, [20, 40], [0, 1], {
+		extrapolateLeft: 'clamp',
+		extrapolateRight: 'clamp',
+	});
+	console.log(stargazersCount);
 	return (
-		<div>
-			<svg
-				style={{transform: `translateY(${wave1}px)`}}
-				className="absolute"
-				height={120}
-				viewBox="0 0 51 31"
-			>
-				<path
-					d="M0 25.364C1.701 18.6 5.954 15.218 12.758 15.218C19.0815 15.218 21.9775 18.1395 24.4794 20.6634L24.48 20.664C26.015 22.213 27.403 23.613 29.344 24.096C32.746 24.941 35.723 23.673 38.275 20.291C36.574 27.055 32.321 30.436 25.517 30.436C19.1925 30.436 16.2975 27.5155 13.7956 24.9916L13.795 24.991C12.26 23.441 10.872 22.041 8.93 21.559C5.529 20.714 2.552 21.982 0 25.364Z"
-					className="fill-sky-400"
-				/>
-			</svg>
-			<svg
-				style={{transform: `translateY(${wave2}px)`}}
-				height={120}
-				viewBox="0 0 51 31"
-			>
-				<path
-					d="M12 10.146C13.701 3.382 17.954 0 24.758 0C31.0815 0 33.9775 2.92151 36.4794 5.44536L36.48 5.446C38.015 6.995 39.403 8.395 41.344 8.878C44.746 9.723 47.723 8.455 50.275 5.073C48.574 11.837 44.321 15.218 37.517 15.218C31.1925 15.218 28.2975 12.2975 25.7956 9.77365L25.795 9.773C24.26 8.223 22.872 6.823 20.93 6.341C17.529 5.496 14.552 6.764 12 10.146Z"
-					className="fill-sky-400"
-				/>
-			</svg>
+		<div
+			style={{opacity}}
+			className="flex flex-row justify-center align-middle text-center border-2 border-black rounded-xl  px-6 py-4 "
+		>
+			<p className="font-bold m-auto text-3xl">{stargazersCount}</p>
+			<div className="p-4 m-auto ">
+				<svg
+					className="absolute -ml-4 -mt-5  pl-2"
+					height={120}
+					viewBox="0 0 51 51"
+				>
+					<path
+						fillRule="evenodd"
+						d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
+					/>
+				</svg>
+			</div>
 		</div>
 	);
 };
